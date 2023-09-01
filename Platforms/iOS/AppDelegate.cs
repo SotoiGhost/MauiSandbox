12a661a9578868c55033bcfdd79d91446458624e
Israel Soto <israel.spgh@gmail.com>
2023-09-01 14:56:01 -0600

    Added initial project

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiSandbox;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
