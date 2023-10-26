ed81779ddeb92bee8f228f294bfabdfa96bfbe7c
Israel Soto <israel.spgh@gmail.com>
2023-10-25 23:32:26 -0600

    Added test case for MCT analyzer issue

--- MauiSandbox/MauiProgram.cs
+++ MauiSandbox/MauiProgram.cs
@@ -1,24 +1,25 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+
+using Microsoft.Extensions.Logging;
 
 namespace MauiSandbox;
 
-public static class MauiProgram
-{
-	public static MauiApp CreateMauiApp()
+public static class MauiProgram {
+	public static MauiApp CreateMauiApp ()
 	{
-		var builder = MauiApp.CreateBuilder();
+		var builder = MauiApp.CreateBuilder ();
 		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			.UseMauiApp<App> ()
+			.UseMauiCommunityToolkitMediaElement ()
+			.ConfigureFonts (fonts => {
+				fonts.AddFont ("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont ("OpenSans-Semibold.ttf", "OpenSansSemibold");
 			});
 
 #if DEBUG
-		builder.Logging.AddDebug();
+		builder.Logging.AddDebug ();
 #endif
 
-		return builder.Build();
+		return builder.Build ();
 	}
 }
