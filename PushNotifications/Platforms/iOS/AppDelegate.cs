ab7ec4b0113386311d304f5f55c36dbe79731810
Israel Soto <israel.spgh@gmail.com>
2025-02-07 15:49:15 -0600

    Handled Push Notifications with Actions on iOS

--- PushNotifications/Platforms/iOS/AppDelegate.cs
+++ PushNotifications/Platforms/iOS/AppDelegate.cs
@@ -5,32 +5,12 @@ using UserNotifications;
 namespace PushNotifications;
 
 [Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate, IUNUserNotificationCenterDelegate
+public class AppDelegate : MauiUIApplicationDelegate
 {
 	protected override MauiApp CreateMauiApp()
 	{
 		return MauiProgram.CreateMauiApp();
 	}
-
-	// [Export("userNotificationCenter:willPresentNotification:withCompletionHandler:")]
-	// public void WillPresentNotification(UNUserNotificationCenter center, UNNotification notification, Action<UNNotificationPresentationOptions> completionHandler)
-	// {
-	// 	Console.WriteLine(nameof(WillPresentNotification));
-	// 	Console.WriteLine(notification.Request.Content.Title);
-	// 	Console.WriteLine(notification.Request.Content.Body);
-
-	// 	completionHandler(UNNotificationPresentationOptions.Badge | UNNotificationPresentationOptions.Banner);
-	// }
-
-	// [Export("userNotificationCenter:didReceiveNotificationResponse:withCompletionHandler:")]
-	// public void DidReceiveNotificationResponse(UNUserNotificationCenter center, UNNotificationResponse response, Action completionHandler)
-	// {
-	// 	Console.WriteLine(nameof(DidReceiveNotificationResponse));
-	// 	Console.WriteLine(response.Notification.Request.Content.Title);
-	// 	Console.WriteLine(response.Notification.Request.Content.Body);
-
-	// 	completionHandler();
-	// }
 }
 
 public class NotificationCenterDelegate : UNUserNotificationCenterDelegate
@@ -41,7 +21,7 @@ public class NotificationCenterDelegate : UNUserNotificationCenterDelegate
 		Console.WriteLine(notification.Request.Content.Title);
 		Console.WriteLine(notification.Request.Content.Body);
 
-		completionHandler(UNNotificationPresentationOptions.Badge | UNNotificationPresentationOptions.Banner);
+		completionHandler(UNNotificationPresentationOptions.Badge | UNNotificationPresentationOptions.Sound | UNNotificationPresentationOptions.Banner);
 	}
 
 	public override void DidReceiveNotificationResponse(UNUserNotificationCenter center, UNNotificationResponse response, Action completionHandler)
@@ -49,6 +29,35 @@ public class NotificationCenterDelegate : UNUserNotificationCenterDelegate
 		Console.WriteLine(nameof(DidReceiveNotificationResponse));
 		Console.WriteLine(response.Notification.Request.Content.Title);
 		Console.WriteLine(response.Notification.Request.Content.Body);
+
+		if (response.Notification.Request.Content.CategoryIdentifier == "NOTIFICATION_WITH_ACTIONS")
+		{
+			if (response.IsCustomAction) {
+				switch (response.ActionIdentifier) {
+				case "ACCEPT_ACTION":
+					Console.WriteLine ("Accept action tapped");
+					UIApplication.SharedApplication.OpenUrl (
+						new NSUrl ("https://www.turtlebeach.com/"),
+						new UIApplicationOpenUrlOptions(),
+						null);
+					break;
+				case "DECLINE_ACTION":
+					Console.WriteLine ("Decline action tapped");
+					break;
+				default:
+					Console.WriteLine ("Unknown action tapped");
+					break;
+				}
+			}
+			else if (response.IsDefaultAction)
+			{
+				Console.WriteLine ("Default action tapped");
+			}
+			else
+			{
+				Console.WriteLine ("Dismiss action tapped");
+			}
+		}
 
 		completionHandler();
 	}
--- PushNotifications/Platforms/iOS/MauiProgram.cs
+++ PushNotifications/Platforms/iOS/MauiProgram.cs
@@ -1,3 +1,5 @@
+using Foundation;
+
 using Microsoft.Maui.LifecycleEvents;
 
 using Plugin.Firebase.CloudMessaging;
@@ -8,19 +10,28 @@ namespace PushNotifications;
 
 public static partial class MauiProgram
 {
-	private static partial MauiAppBuilder RegisterFirebaseServices(this MauiAppBuilder builder)
+    private static partial MauiAppBuilder RegisterFirebaseServices(this MauiAppBuilder builder)
     {
         builder.ConfigureLifecycleEvents(events => {
             events.AddiOS(iOS => { 
                 iOS.WillFinishLaunching((_, __) => {
                     CrossFirebase.Initialize();
                     FirebaseCloudMessagingImplementation.Initialize();
-                    return false;
+                    return true;
                 });
 
                 iOS.FinishedLaunching((_, __) => {
-		            UNUserNotificationCenter.Current.Delegate = new NotificationCenterDelegate();
-                    return false;
+					var userNotificationCenter = UNUserNotificationCenter.Current;
+                    userNotificationCenter.Delegate = new NotificationCenterDelegate();
+
+					var acceptAction = UNNotificationAction.FromIdentifier("ACCEPT_ACTION", "Open TB Website", UNNotificationActionOptions.Foreground);
+					var declineAction = UNNotificationAction.FromIdentifier("DECLINE_ACTION", "Decline", UNNotificationActionOptions.None);
+
+					var notificationWithActions = UNNotificationCategory.FromIdentifier("NOTIFICATION_WITH_ACTIONS", [acceptAction, declineAction], [], UNNotificationCategoryOptions.CustomDismissAction);
+
+					userNotificationCenter.SetNotificationCategories(new NSSet<UNNotificationCategory>(notificationWithActions));
+
+                    return true;
                 });
             });
         });
